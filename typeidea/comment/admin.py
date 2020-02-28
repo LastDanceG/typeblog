41f2782c4fa0745db779ee781ccfc7dbfb98aacb
lbs <ail>
2020-02-28 15:43:04 +0800

    添加comment app中的admin以及model的__unicode__和__str__方法

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Comment
+
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-from typeidea.blog.models import Post
+from blog.models import Post
 
 
 class Comment(models.Model):
@@ -15,6 +15,12 @@ class Comment(models.Model):
     website = models.URLField(verbose_name="网站")
     email = models.EmailField(verbose_name="邮箱")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    def __unicode__(self):
+        return self.nickname
+
+    def __str__(self):
+        return self.nickname
 
     class Meta:
         verbose_name = verbose_name_plural = "评论"
