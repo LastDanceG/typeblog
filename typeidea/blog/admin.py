9a28b8834ebda8a18fcc0858e6387b4d36119691
lbs <ail>
2020-03-03 11:52:56 +0800

    将blog中的分类和标签model 注册到admin中

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -5,16 +5,17 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from typeidea.custom_admin import BaseOwnerAdmin
 from typeidea.settings.custom_site import custom_site
 from .models import Post, Category, Tag
 # Register your models here.
 
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
 
     list_display = [
-        'title', 'category', 'status', 'owner',
+        'title', 'category', 'status',
         'create_time', 'update_time', 'operator'
     ]
     search_fields = ['owner__username', 'category__name', 'title']
@@ -31,7 +32,7 @@ class PostAdmin(admin.ModelAdmin):
     # 和fields互斥
     fieldsets = (
         ('基础配置', {
-            'fields': (('title', 'owner'), ('category', 'status'), 'desc', 'content')
+            'fields': (('title',), ('category', 'status'), 'desc', 'content')
         }),
         ('高级配置', {
             'classes': ('collapse', 'addon'),
@@ -49,14 +50,31 @@ class PostAdmin(admin.ModelAdmin):
     operator.short_description = "操作"
 
 
+class PostInline(admin.TabularInline):   # TabularInline 为横向展示。 StackedInline 为竖向展示
+    fields = ('title', 'desc', 'status')
+    extra = 1  # 默认为4个，即最多展示4篇文章
+    model = Post
+
+
 @admin.register(Category, site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
-    pass
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline]
+    list_display = ['name', 'status', 'is_nav', 'create_time']
+    search_fields = ['name']
+    actions_on_top = True
+    actions_on_bottom = True
+    date_hierarchy = 'create_time'
+    list_editable = ['status']
 
 
 @admin.register(Tag, site=custom_site)
-class TagAdmin(admin.ModelAdmin):
-    pass
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ['name', 'status', 'create_time']
+    search_fields = ['name']
+    actions_on_top = True
+    actions_on_bottom = True
+    date_hierarchy = 'create_time'
+    list_editable = ['status']
 
 
 
