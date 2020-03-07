b125c18943e38522da79d681d5b44d3aefc89382
lbs <ail>
2020-03-07 20:58:04 +0800

    替换admin为xadmin

--- typeidea/config/adminx.py
+++ typeidea/config/adminx.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+import xadmin
+# from django.contrib import admin
 
 from typeidea.custom_admin import BaseOwnerAdmin
-from typeidea.settings.custom_site import custom_site
+# from typeidea.custom_site import custom_site
 from .models import Link, SideBar
 
 # Register your models here.
 
 
-@admin.register(Link, site=custom_site)
 class LinkAdmin(BaseOwnerAdmin):
     list_display = ['title', 'href', 'status', 'weight', 'create_time']
     search_fields = ['title', 'status', 'weight']
@@ -20,7 +20,6 @@ class LinkAdmin(BaseOwnerAdmin):
     list_editable = ['status']
 
 
-@admin.register(SideBar, site=custom_site)
 class SideBarAdmin(BaseOwnerAdmin):
     list_display = ['title', 'status', 'display_type', 'create_time']
     search_fields = ['title', 'status']
@@ -28,3 +27,6 @@ class SideBarAdmin(BaseOwnerAdmin):
     actions_on_bottom = True
     date_hierarchy = 'create_time'
     list_editable = ['display_type']
+
+xadmin.site.register(Link, LinkAdmin)
+xadmin.site.register(SideBar, SideBarAdmin)
