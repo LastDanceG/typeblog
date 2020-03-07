4c8d1edeaa3b4dffd253a89e25f2e4840f63823c
lbs <ail>
2020-03-07 20:59:25 +0800

    添加友链页面

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import ListView
+
+from blog.views import CommonMixin
+from comment.views import CommentShowMixin
+from config.models import Link
+
+
+class LinkView(CommonMixin, CommentShowMixin, ListView):
+
+    queryset = Link.objects.filter(status=1)
+    template_name = 'config/links.html'
+    context_object_name = 'links'
