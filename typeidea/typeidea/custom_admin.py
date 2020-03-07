c64226a89896eb8b4b46c3161e5c7f4c7ab36f7d
lbs <ail>
2020-03-07 21:08:53 +0800

    替换xadmin后修改baseadmin类，针对有owner属性的数据重写save_model和get_queryset方法

--- typeidea/typeidea/custom_admin.py
+++ typeidea/typeidea/custom_admin.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
+import xadmin
+from xadmin.views import CommAdminView
 
-from django.contrib import admin
 
-
-class BaseOwnerAdmin(admin.ModelAdmin):
+class BaseOwnerAdmin(object):
     """针对有owner属性的数据，重写
     1、save_model - 保证每条数据都属于当前登录的用户
     2、重写get_queryset - 保证每个用户只能看见自己的文章
     """
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(BaseOwnerAdmin, self).save_model(request, obj, form, change)
+    def save_models(self):
+        if not self.org_obj:
+            self.new_obj.owner = self.request.user
+        return super(BaseOwnerAdmin, self).save_models()
 
-    def get_queryset(self, request):
-        qs = super(BaseOwnerAdmin, self).get_queryset(request)
+    def get_list_queryset(self):
+        request = self.request
+        qs = super(BaseOwnerAdmin, self).get_list_queryset()
         if request.user.is_superuser:
             return qs
         return qs.filter(owner=request.user)
+
+
+class GlobalSetting(CommAdminView):
+
+    site_title = 'UTF-8'
+    site_footer = 'power by libsh'
+
+xadmin.site.register(CommAdminView, GlobalSetting)
