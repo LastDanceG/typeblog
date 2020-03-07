5e0ef3e5c4e8358d9ff0aa2c35ad34ffc3c25c1c
lbs <ail>
2020-03-07 20:51:39 +0800

    替换admin为xadmin

--- typeidea/comment/adminx.py
+++ typeidea/comment/adminx.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import xadmin
 from django.contrib import admin
 
-from typeidea.settings.custom_site import custom_site
+# from typeidea.custom_site import custom_site
+from typeidea.custom_admin import BaseOwnerAdmin
 from .models import Comment
 
 # Register your models here.
 
 
-@admin.register(Comment, site=custom_site)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['post', 'nickname', 'status', 'website', 'email', 'create_time']
+class CommentAdmin(object):
+    list_display = ['target', 'nickname', 'status', 'website', 'email', 'create_time']
     search_fields = ['nickname', 'status']
     actions_on_top = True
     actions_on_bottom = True
     date_hierarchy = 'create_time'
+
+xadmin.site.register(Comment, CommentAdmin)
