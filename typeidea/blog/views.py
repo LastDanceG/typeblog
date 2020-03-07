e868b8f05c2694f94eaa94c98c31391f97c9e7b7
lbs <ail>
2020-03-07 20:47:39 +0800

    将function view 替换成class view 并且增加追着页以及pv和uv的统计

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -2,16 +2,140 @@
 from __future__ import unicode_literals
 
 # from django.http import HttpResponse
+from django.core.cache import cache
 from django.core.paginator import Paginator, EmptyPage
+from django.db.models import F
 from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from blog.models import Post, Tag, Category
+from comment.forms import CommentForm
 from comment.models import Comment
+from comment.views import CommentShowMixin
 from config.models import SideBar
 
 
+class CommonMixin(object):
+
+    def get_category_context(self):
+        categories = Category.objects.filter(status=1)
+
+        nov_cate = []
+        cate = []
+
+        for category in categories:
+            if category.is_nav:
+                nov_cate.append(category)
+            else:
+                cate.append(category)
+        return {
+            'nav_cate': nov_cate,
+            'cate': cate,
+        }
+    
+    def get_context_data(self, **kwargs):
+
+        recently_posts = Post.objects.filter(status=1)[:10]
+        recently_comment = Comment.objects.filter(status=1)[:3]
+        host_posts = Post.objects.filter(status=1).order_by('-pv')[0:10]
+        side_bar = SideBar.objects.filter(status=1)
+
+        kwargs.update({
+            'recently_posts': recently_posts,
+            'recently_comment': recently_comment,
+            'side_bar': side_bar,
+            'host_posts': host_posts,
+        })
+        kwargs.update(self.get_category_context())
+        return super(CommonMixin, self).get_context_data(**kwargs)
+        
+
+class BasePostsView(CommonMixin, ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+
+class IndexView(BasePostsView):
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        qs = super(IndexView, self).get_queryset()
+        if query:
+            qs = qs.filter(title__icontains=query)  # "select * from post where title ilike '%{}%'".format(query)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        query = self.request.GET.get('query')
+        return super(IndexView, self).get_context_data(query=query)
+
+
+class CategoryView(BasePostsView):
+    def get_queryset(self):
+        qs = super(CategoryView, self).get_queryset()
+        cate_id = self.kwargs.get('category_id')
+        category = qs.filter(category_id=cate_id)
+        return category
+
+
+class TagView(BasePostsView):
+
+    def get_queryset(self):
+        tag_id = self.kwargs.get('tag_id')
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            queryset = []
+        else:
+            queryset = tag.post_set.all()
+        return queryset
+
+
+class AuthorView(BasePostsView):
+
+    def get_queryset(self):
+        author_id = self.kwargs.get('author_id')
+
+        qs = super(AuthorView, self).get_queryset()
+        if author_id:
+            qs = qs.filter(owner_id=author_id)
+        return qs
+
+
+class PostView(CommonMixin, CommentShowMixin, DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+
+        response = super(PostView, self).get(request, *args, **kwargs)
+        self.pv_uv()
+        return response
+
+    def pv_uv(self):
+
+        session_id = self.request.COOKIES.get('sessionid')
+        if not session_id:
+            return
+
+        # 增加pv 判断用户是否在60s内访问过
+        pv_key = 'pv:{}:{}'.format(session_id, self.request.path)
+        if not cache.get(pv_key):
+            self.object.increase_pv()
+            cache.set(pv_key, 1, 60)
+
+        # 增加uv，判断用户是否在24小时内访问过
+        uv_key = 'uv:{}:{}'.format(session_id, self.request.path)
+        if not cache.get(uv_key):
+            self.object.increase_uv()
+            cache.set(uv_key, 1, 60 * 60 * 24)
+
+
+# ------------------------------以下为FUNCTION VIEW，已弃用-------------------------------------
 def get_common_context():
     categories = Category.objects.all()
 
