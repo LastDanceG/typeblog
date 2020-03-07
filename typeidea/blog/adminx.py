f90dcb700fb55eea30e9afc09a24bf112e0578d5
lbs <ail>
2020-03-07 20:44:02 +0800

    添加xadmin 后台管理

--- typeidea/blog/adminx.py
+++ typeidea/blog/adminx.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+import xadmin
+# from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from xadmin.layout import Fieldset, Row
 
+from blog.adminforms import PostAdminForm
 from typeidea.custom_admin import BaseOwnerAdmin
-from typeidea.settings.custom_site import custom_site
 from .models import Post, Category, Tag
 # Register your models here.
 
 
-@admin.register(Post, site=custom_site)
 class PostAdmin(BaseOwnerAdmin):
 
+    form = PostAdminForm
     list_display = [
-        'title', 'category', 'status',
+        'title', 'category', 'status', 'pv', 'uv',
         'create_time', 'update_time', 'operator'
     ]
     search_fields = ['owner__username', 'category__name', 'title']
@@ -30,14 +32,28 @@ class PostAdmin(BaseOwnerAdmin):
 
     # 编辑界面
     # 和fields互斥
-    fieldsets = (
-        ('基础配置', {
-            'fields': (('title',), ('category', 'status'), 'desc', 'content')
-        }),
-        ('高级配置', {
-            'classes': ('collapse', 'addon'),
-            'fields': ('tags',)
-        })
+    # fieldsets = (
+    #     ('基础配置', {
+    #         'fields': (('title',), ('category', 'status'), 'desc', ('content', 'is_markdown'), 'html')
+    #     }),
+    #     ('高级配置', {
+    #         'classes': ('collapse', 'addon'),
+    #         'fields': ('tags',)
+    #     })
+    # )
+
+    exclude = ['html', 'pv', 'uv', 'owner']
+
+    form_layout = (
+        Fieldset(
+            '基础信息',
+            'title',
+            'desc',
+            Row('category', 'status'),
+            'is_markdown',
+            Row('content'),
+            'tags',
+        ),
     )
 
     filter_horizontal = ('tags',)
@@ -45,20 +61,19 @@ class PostAdmin(BaseOwnerAdmin):
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('cus_admin:blog_post_change', args=(obj.id,))
+            reverse('admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = "操作"
 
 
-class PostInline(admin.TabularInline):   # TabularInline 为横向展示。 StackedInline 为竖向展示
-    fields = ('title', 'desc', 'status')
-    extra = 1  # 默认为4个，即最多展示4篇文章
-    model = Post
+# class PostInline(admin.TabularInline):   # TabularInline 为横向展示。 StackedInline 为竖向展示
+#     fields = ('title', 'desc', 'status')
+#     extra = 1  # 默认为4个，即最多展示4篇文章
+#     model = Post
 
 
-@admin.register(Category, site=custom_site)
 class CategoryAdmin(BaseOwnerAdmin):
-    inlines = [PostInline]
+    # inlines = [PostInline]
     list_display = ['name', 'status', 'is_nav', 'create_time']
     search_fields = ['name']
     actions_on_top = True
@@ -67,7 +82,6 @@ class CategoryAdmin(BaseOwnerAdmin):
     list_editable = ['status']
 
 
-@admin.register(Tag, site=custom_site)
 class TagAdmin(BaseOwnerAdmin):
     list_display = ['name', 'status', 'create_time']
     search_fields = ['name']
@@ -77,4 +91,6 @@ class TagAdmin(BaseOwnerAdmin):
     list_editable = ['status']
 
 
-
+xadmin.site.register(Post, PostAdmin)
+xadmin.site.register(Category, CategoryAdmin)
+xadmin.site.register(Tag, TagAdmin)
