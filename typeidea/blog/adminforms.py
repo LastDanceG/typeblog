7a1d5a303203d4679548f2722f106329812d95c4
lbs <ail>
2020-03-07 20:43:03 +0800

    添加ckeditor文本编辑器的form

---
+++ typeidea/blog/adminforms.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from dal import autocomplete
+from django import forms
+
+from blog.models import Category, Tag
+
+
+class PostAdminForm(forms.ModelForm):
+
+    desc = forms.CharField(widget=forms.Textarea, label="摘要", required=False)
+    content = forms.CharField(widget=CKEditorUploadingWidget(), label="内容")
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        widget=autocomplete.ModelSelect2(url='category-autocomplete'),
+        label="分类",
+    )
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=autocomplete.ModelSelect2Multiple(url='tag-autocomplete'),
+        label="标签",
+    )
