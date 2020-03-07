fdd043546813866669c004bc8d8aedbfcfa326f2
lbs <ail>
2020-03-07 21:19:43 +0800

    url配置

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
-"""typeidea URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
-from django.contrib import admin
 
-from typeidea.settings.custom_site import custom_site
-from blog.views import post_list, post_detail
-# from config import links
+import xadmin
+from xadmin.plugins import xversion
+from ckeditor_uploader import urls as uploader_urls
+
+from typeidea.autocomplete import CategoryAutocomplete, TagAutocomplete
+from blog.views import IndexView, TagView, CategoryView, PostView, AuthorView
+from comment.views import CommentView
+from config.views import LinkView
+# from typeidea.custom_admin import BaseOwnerAdmin  #NOQA
+xadmin.autodiscover()
+xversion.register_models()
 
 urlpatterns = [
-    url(r'^$', post_list, name='index'),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category'),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag'),
-    url(r'post/(?P<post_id>\d+)/$', post_detail, name='detail'),
-    # url(r'^links/$', links),
-    url(r'^admin/', admin.site.urls),
-    url(r'^cus_admin/', custom_site.urls),
-]
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag'),
+    url(r'post/(?P<pk>\d+)/$', PostView.as_view(), name='detail'),
+    url(r'author/(?P<author_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$', LinkView.as_view(), name='link'),
+    url(r'^comment/$', CommentView.as_view(), name='comment'),
+    url(r'^admin/', xadmin.site.urls),
+    url(r'^category-autocomplete/$', CategoryAutocomplete.as_view(), name='category-autocomplete'),
+    url(r'^tag-autocomplete/$', TagAutocomplete.as_view(), name='tag-autocomplete')
+] + uploader_urls.urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
