b7449dc29f5d50b0c2b253ea63acda6485e3f9cf
lbs <ail>
2020-03-16 12:51:37 +0800

    添加所需要的第三方库以及版本号

--- setup.py
+++ setup.py
@@ -20,6 +20,21 @@ setup(
     include_package_data=True,  # 方法二  ，配置MANIFEST.in文件
     install_requires=[
         'django==1.11.3',
+        'django-autocomplete-light==3.2.10',
+        'hiredis==1.0.1'
+        'redis==2.10.6',
+        'django-ckeditor==5.3.1',
+        'django-debug-toolbar==1.11',
+        'mysqlclient==1.4.6',
+        'Pillow==4.3.0',
+        'djangorestframework==3.7.0',
+        'django-reversion==2.0.10',
+        'Markdown==2.6.9',
+        'django-redis==4.8.0',
+        'django-import-export==0.5.1',
+        'requests==2.23.0',
+        'six==1.14.0',
+        'gunicorn==19.7.1'
     ],
     scripts=[
         'typeidea/manage.py',
